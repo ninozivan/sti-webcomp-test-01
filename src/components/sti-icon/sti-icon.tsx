5d7d631d91c8f18a581b3f04982fe5b3b80a6f00
Ninoslav Zivanovic <nzivanovic@showingtime.com>
2021-04-28 15:39:13 +0200

    Completed logic of icon loading process
    I've followed this article when assembling these changes.
    https://paulcpederson.com/articles/stencil-icons/
    
    There are some good advices for caching and the loading process.

--- src/components/sti-icon/sti-icon.tsx
+++ src/components/sti-icon/sti-icon.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, State, Watch, Prop } from '@stencil/core';
+import { Build, Component, Element, h, Host, Prop, State, Watch } from '@stencil/core';
 import { fetchIcon } from '../../utils/utils';
 
 @Component({
@@ -7,13 +7,26 @@ import { fetchIcon } from '../../utils/utils';
   shadow: true,
 })
 export class StiIcon {
+  @Element() el: HTMLElement;
   @Prop() icon: string = null;
 
-  @State() private svgContent?: string;
+  @State() private svgContent: string;
+  @State() private visible = false;
 
-  @Watch('icon') private async loadIconPathData(): Promise<void> {
-    const { icon } = this;
-    this.svgContent = await fetchIcon({ icon });
+  private intersectionObserver: IntersectionObserver;
+
+  connectedCallback(): void {
+    this.waitUntilVisible(() => {
+      this.visible = true;
+      this.loadIconPathData();
+    });
+  }
+
+  disconnectedCallback(): void {
+    if (this.intersectionObserver) {
+      this.intersectionObserver.disconnect();
+      this.intersectionObserver = null;
+    }
   }
 
   async componentWillLoad(): Promise<void> {
@@ -22,5 +35,37 @@ export class StiIcon {
 
   render() {
     return <Host role="img">{this.svgContent ? <div class="icon-wrapper" innerHTML={this.svgContent}></div> : <div class="icon-wrapper"></div>}</Host>;
+  }
+
+  @Watch('icon') private async loadIconPathData(): Promise<void> {
+    const { icon, visible } = this;
+
+    if (!Build.isBrowser || !icon || !visible) {
+      return;
+    }
+
+    this.svgContent = await fetchIcon({ icon });
+  }
+
+  private waitUntilVisible(callback: () => void): void {
+    if (!Build.isBrowser || typeof window === 'undefined' || !(window as any).IntersectionObserver) {
+      callback();
+      return;
+    }
+
+    this.intersectionObserver = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.intersectionObserver.disconnect();
+            this.intersectionObserver = null;
+            callback();
+          }
+        });
+      },
+      { rootMargin: '50px' },
+    );
+
+    this.intersectionObserver.observe(this.el);
   }
 }
